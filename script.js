eb4ea0ca8b47d4bb508b8f48a5f0457b4af9f1ab
Veysel Bozoglu <aveyselbozoglu@gmail.com>
2022-02-07 21:16:32 +0300

    tıklanan kart tıklandığında dönme özelliği eklendi

--- script.js
+++ script.js
@@ -1,8 +1,19 @@
-let kutuSayisi = 9;
+let kutuSayisi = 4;
+
+const imageUrls = [
+  "https://i.picsum.photos/id/669/200/200.jpg?hmac=lAa_bMRK0BRBCTEvl1acVqTfEDrXQc0yNwi683-13cE",
+  "https://i.picsum.photos/id/202/200/200.jpg?hmac=eGzhW5P2k0gzjc76Tk5T9lOfvn30h3YHuw5jGnBUY4Y",
+];
 
 document.addEventListener("DOMContentLoaded", function (event) {
+  setupBoard();
   createCards();
 });
+
+const setupBoard = () => {
+  let score = document.querySelector("#score");
+  score.innerHTML += "0";
+};
 
 function createCards() {
   for (let i = 0; i < kutuSayisi; i++) {
@@ -15,12 +26,32 @@ function createCards() {
     card.className = "card";
     front.className = "front";
     back.className = "back";
+    card.setAttribute("id", "card-" + i.toString());
+    card.onclick = function () {
+      cl(card);
+    };
 
-    front.append("Deneme");
-    back.append("Deneme2");
+    if (i % 2 === 0) back.innerHTML += "<img src=" + imageUrls[0] + " />";
+    else back.innerHTML += "<img src=" + imageUrls[1] + " />";
+
+    back.firstElementChild.classList.add("image-style");
     card.appendChild(front);
     card.appendChild(back);
     cardWrapper.appendChild(card);
     mainGrid.appendChild(cardWrapper);
   }
+}
+
+function cl(card) {
+  const clickedCardId = card.getAttribute("id");
+  console.log(clickedCardId);
+  const clickedCard = document.querySelector("#" + clickedCardId);
+
+  const clickedCardFront = clickedCard.firstElementChild;
+  const clickedCardBack = clickedCard.lastElementChild;
+
+  clickedCardFront.classList.add("clickedCardFront");
+  clickedCardBack.classList.add("clickedCardBack");
+
+  console.log(clickedCardFront);
 }
