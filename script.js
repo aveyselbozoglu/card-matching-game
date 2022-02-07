acca81fae4b41e1146536c04d2551edf184bd190
Veysel Bozoglu <aveyselbozoglu@gmail.com>
2022-02-07 21:51:44 +0300

    random cards created done

--- script.js
+++ script.js
@@ -1,14 +1,28 @@
-let kutuSayisi = 4;
+let kutuSayisi = 8;
+
+let randomNumbers = [];
 
 const imageUrls = [
   "https://i.picsum.photos/id/669/200/200.jpg?hmac=lAa_bMRK0BRBCTEvl1acVqTfEDrXQc0yNwi683-13cE",
   "https://i.picsum.photos/id/202/200/200.jpg?hmac=eGzhW5P2k0gzjc76Tk5T9lOfvn30h3YHuw5jGnBUY4Y",
+  "https://i.picsum.photos/id/511/200/200.jpg?hmac=QTzrMGu9nrJDRE4TMoboI_EAM5ZdwXF09ylHr7LFZCg",
+  "https://i.picsum.photos/id/352/200/200.jpg?hmac=HPgFQ0Sto_7261sbYIaRW0-z2Jq0-C92RSt0vkdC6Uc",
 ];
 
 document.addEventListener("DOMContentLoaded", function (event) {
+  fillRandoms(kutuSayisi);
   setupBoard();
   createCards();
 });
+
+const fillRandoms = (kutuSayisi) => {
+  let flatArray = [];
+  for (let i = 0; i < kutuSayisi / 2; i++) {
+    flatArray.push(i);
+    flatArray.push(i);
+  }
+  randomNumbers = flatArray.sort(() => Math.random() - 0.5);
+};
 
 const setupBoard = () => {
   let score = document.querySelector("#score");
@@ -20,31 +34,32 @@ function createCards() {
     let mainGrid = document.querySelector(".main-grid");
     let cardWrapper = document.createElement("div");
     let card = document.createElement("a");
-    let front = document.createElement("front");
-    let back = document.createElement("front");
+    let front = document.createElement("div");
+    let back = document.createElement("div");
     cardWrapper.className = "card-wrapper";
     card.className = "card";
     front.className = "front";
     back.className = "back";
-    card.setAttribute("id", "card-" + i.toString());
-    card.onclick = function () {
-      cl(card);
-    };
 
-    if (i % 2 === 0) back.innerHTML += "<img src=" + imageUrls[0] + " />";
-    else back.innerHTML += "<img src=" + imageUrls[1] + " />";
+    back.innerHTML += "<img src=" + imageUrls[randomNumbers[i]] + " />";
+    //back.innerHTML += "<img src=" + imageUrls[random] + " />";
 
     back.firstElementChild.classList.add("image-style");
     card.appendChild(front);
     card.appendChild(back);
     cardWrapper.appendChild(card);
     mainGrid.appendChild(cardWrapper);
+
+    card.setAttribute("id", "card-" + i.toString());
+    card.onclick = function () {
+      clickCard(card);
+    };
   }
 }
 
-function cl(card) {
+function clickCard(card) {
   const clickedCardId = card.getAttribute("id");
-  console.log(clickedCardId);
+
   const clickedCard = document.querySelector("#" + clickedCardId);
 
   const clickedCardFront = clickedCard.firstElementChild;
@@ -52,6 +67,4 @@ function cl(card) {
 
   clickedCardFront.classList.add("clickedCardFront");
   clickedCardBack.classList.add("clickedCardBack");
-
-  console.log(clickedCardFront);
 }
